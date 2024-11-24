0adcbfe7266914a5175791ae0647de5c0487d8f8
Your Name <you@example.com>
2024-11-24 14:56:29 +0200

    POMmanager

--- tests/TestsPage.spec.js
+++ tests/TestsPage.spec.js
@@ -1,39 +1,34 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 const { Utils } = require("../Utilities/Utils");
-const { POMmanager } = require("../pages.js/POMnanager")
+const { POMmanager } = require("../pages.js/POMnanager");
 
-
-
-test.describe('Tests', () => {
-
-    test.beforeEach(async ({ page }) => {
-      test.step('more details in the repport', async () => {
-      
+test.describe("Tests", () => {
+  test.beforeEach(async ({ page }) => {
+    test.step("more details in the repport", async () => {
       const pommanager = new POMmanager(page);
       const login = pommanager.getLogin();
       await page.goto(Utils.User_Details.BaseUrl);
-      await login.performLogin(Utils.User_Details.Username, Utils.User_Details.Password);
-    
-    })
-    
-});
+      await login.performLogin(
+        Utils.User_Details.Username,
+        Utils.User_Details.Password
+      );
+    });
+  });
 
-test.afterEach(async({page}, testInfo) => {
+  test.afterEach(async ({ page }, testInfo) => {
     await page.waitForTimeout(500);
-    await page.screenshot({path: `screenshots/${testInfo.title}_${testInfo.status}.jpg`})
-})
-  
+    await page.screenshot({
+      path: `screenshots/${testInfo.title}_${testInfo.status}.jpg`,
+    });
+  });
 
-test('Validate Login Test', async ({ page }) => {
-    
-    await page.waitForURL(Utils.User_Details.LoggedUrl)
-    expect(page).toHaveURL(Utils.User_Details.LoggedUrl)
+  test("Validate Login Test", async ({ page }) => {
+    await page.waitForURL(Utils.User_Details.LoggedUrl);
+    expect(page).toHaveURL(Utils.User_Details.LoggedUrl);
+  });
 
-});
-
-test('Full Flow', async ({page}) => {
-
+  test("Full Flow", async ({ page }) => {
     const pommanager = new POMmanager(page);
     const inven = pommanager.getInvenv();
     const item = pommanager.getItem();
@@ -43,15 +38,15 @@ test('Full Flow', async ({page}) => {
     await inven.choseItem();
     await item.addItem();
     await item.goToBasket();
-    await expect(sum.itemTitle).toContainText(Utils.Item_Name)
-    await sum.goToChckOut()
-    await checkout.personalDetails(Utils.User_Details.First_Name, Utils.User_Details.Last_Name, Utils.User_Details.Zip_Code);
+    await expect(sum.itemTitle).toContainText(Utils.Item_Name);
+    await sum.goToChckOut();
+    await checkout.personalDetails(
+      Utils.User_Details.First_Name,
+      Utils.User_Details.Last_Name,
+      Utils.User_Details.Zip_Code
+    );
     await expect(overview.itemTitle).toContainText(Utils.Item_Name);
-    await overview.goFinish()
-    await expect(overview.success).toContainText("Thank you for your order!")
-
-
-
+    await overview.goFinish();
+    await expect(overview.success).toContainText("Thank you for your order!");
+  });
 });
-
-})
